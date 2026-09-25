c23cd5d4dfd856f0b1e33c8d88bb790c9467f62d
Jannik Stollenwerk <jannik-stollenwerk@web.de>
2026-09-25 20:03:28 +0200

    Fix layout syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -21,7 +22,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="de
+      lang="de"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
